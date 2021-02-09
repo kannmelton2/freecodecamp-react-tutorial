c43b0ba8056ed3ed37f161f1e10b1c7264dc3f91
Kayla Melton <kannmelton2@gmail.com>
2021-02-09 15:23:12 -0600

    created first component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 function Greeting() {
   // while we write very familiar html here, we are techincally returning JSX
   return(
-    <h4>This is Kayla and this is my first component</h4>
+    <h4>Our first component</h4>
   )
 }
 
