531491fa7661e5825c2b805bace5d3b359568ff1
Kayla Melton <kannmelton2@gmail.com>
2021-02-09 18:28:33 -0600

    made things a little cleaner by using map method

--- src/index.js
+++ src/index.js
@@ -3,33 +3,42 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-const firstBook = {
-  title: 'Curves for the Mathematically Curious',
-  imageSrc: 'https://images-na.ssl-images-amazon.com/images/I/51fr9AhktcL._SX329_BO1,204,203,200_.jpg',
-  author: 'Julian Havil'
-}
-
-const secondBook = {
-  title: 'Schrodinger Equation',
-  imageSrc: 'https://images-na.ssl-images-amazon.com/images/I/41NooCklH9L._SX329_BO1,204,203,200_.jpg',
-  author: 'Daniel A. Fleisch'
-}
+const books = [
+  {
+    id: 'book1',
+    title: 'Curves for the Mathematically Curious',
+    imageSrc: 'https://images-na.ssl-images-amazon.com/images/I/51fr9AhktcL._SX329_BO1,204,203,200_.jpg',
+    author: 'Julian Havil'
+  },
+  {
+    id: 'book2',
+    title: 'Schrodinger Equation',
+    imageSrc: 'https://images-na.ssl-images-amazon.com/images/I/41NooCklH9L._SX329_BO1,204,203,200_.jpg',
+    author: 'Daniel A. Fleisch'
+  }
+]
 
 function BookList() {
+  const createBooks = (
+    books.map((singleBook) => {
+    return <Book key={singleBook.id} book={singleBook} />
+  })
+  )
+
   return(
     <section className="booklist">
-      <Book imageSrc={firstBook.imageSrc} title={firstBook.title} author={firstBook.author}/>
-      <Book imageSrc={secondBook.imageSrc} title={secondBook.title} author={secondBook.author}/>
+      {createBooks}
     </section>
   )
 }
 
 const Book = (props) => {
+  const { book } = props;
   return (
     <article className="book">
-      <img src={props.imageSrc} alt="book cover" />
-      <p className="title">{props.title}</p>
-      <p className="author">{props.author}</p>
+      <img src={book.imageSrc} alt="book cover" />
+      <p className="title">{book.title}</p>
+      <p className="author">{book.author}</p>
     </article>
   )
 }
